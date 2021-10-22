e59c948e206e6e6de68884c8d0d72a2be3cf0d53
takumhonde9 <tmhonde45@gmail.com>
2021-10-22 12:54:47 -0700

    add dynamic routing

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'James Dean';
+const name = 'Ren Cal';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
